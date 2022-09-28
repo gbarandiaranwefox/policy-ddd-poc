c337f17d20f61455ffbcb316af1807b90eafa767
Javier Nogales <javier.nogales@wefox.com>
2022-09-28 15:52:55 +0200

    refactor: delete unused folders and files

--- tests/Contexts/Shared/domain/Repeater.ts
+++
@@ -1,8 +0,0 @@
-import { IntegerMother } from './IntegerMother';
-export class Repeater {
-  static random(callable: Function, iterations: number) {
-    return Array(iterations || IntegerMother.random(20))
-      .fill({})
-      .map(() => callable());
-  }
-}
